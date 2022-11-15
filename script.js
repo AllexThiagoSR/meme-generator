b5733aad457c05f63c01e2cac592596d07448db0
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-15 14:43:26 -0300

    Cumpre requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const input = document.getElementById('text-input');
+const memeInsert = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text');
+const memeImage = document.getElementById('meme-image');
 
+console.log(memeImage.src);
 input.addEventListener('keyup', () => {
+  // if (memeImage.src === 'http://127.0.0.1:5500/') {
+  //   memeText.style.color = 'black';
+  // } else {
+  //   memeText.style.color = 'white';
+  // }
   memeText.innerText = input.value;
-})
+});
+
+memeInsert.addEventListener('change', () => {
+  memeImage.src = URL.createObjectURL(memeInsert.files[0]);
+});
