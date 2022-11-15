710f905ad9d35f3ce83b0acb0d1f1fe748f689a9
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-15 17:26:52 -0300

    As imagens de exemplo quando clicadas são exibidas no preview

--- script.js
+++ script.js
@@ -5,23 +5,24 @@ const memeImage = document.getElementById('meme-image');
 const memeContainer = document.getElementById('meme-image-container');
 const buttonsSection = document.getElementById('buttons-section');
 const tamplates = document.getElementById('templates');
-const imagesPath = ['images/download(1).jpeg', 'images/download.jpeg', 'images/Screenshot.png', 'images/weird.webp'];
+// const imagesPath = ['imgs/download(1).jpeg', 'imgs/download.jpeg', 'imgs/Screenshot.png', 'imgs/weird.webp'];
+const imagesPath = ['imgs/meme1.png', 'imgs/meme2.png', 'imgs/meme3.png', 'imgs/meme4.png'];
 const buttons = [
-{
-  id: 'fire',
-  border: '3px dashed red',
-  'background-color': 'red',
-},
-{
-  id: 'water', 
-  border: '5px double blue',
-  'background-color': 'blue',
-},
-{
-  id: "earth",
-  border: '6px groove rgb(0, 128, 0)',
-  'background-color': 'rgb(0, 128, 0)',
-},
+  {
+    id: 'fire',
+    border: '3px dashed red',
+    'background-color': 'red',
+  },
+  {
+    id: 'water',
+    border: '5px double blue',
+    'background-color': 'blue',
+  },
+  {
+    id: 'earth',
+    border: '6px groove rgb(0, 128, 0)',
+    'background-color': 'rgb(0, 128, 0)',
+  },
 ];
 
 input.addEventListener('keyup', () => {
@@ -35,7 +36,7 @@ memeInsert.addEventListener('change', () => {
 for (let index = 0; index < buttons.length; index += 1) {
   const button = document.createElement('button');
   button.id = buttons[index].id;
-  button.style.backgroundColor = buttons[index]["background-color"];
+  button.style.backgroundColor = buttons[index]['background-color'];
   button.addEventListener('click', () => {
     memeContainer.style.border = buttons[index].border;
   });
@@ -46,7 +47,11 @@ for (let index = 0; index < buttons.length; index += 1) {
 
 for (let index = 0; index < imagesPath.length; index += 1) {
   const img = document.createElement('img');
-  img.id = 'meme-' + (index + 1);
+  img.id = `meme-${(index + 1)}`;
   img.src = imagesPath[index];
+  img.addEventListener('click', (event) => {
+    const imgClicked = event.target;
+    memeImage.src = imgClicked.src;
+  });
   tamplates.appendChild(img);
 }
