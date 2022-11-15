a56ece64a65600ce4a36987d9c0ee9b4ae0e9f7f
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-15 14:55:01 -0300

    Seta a imagem com position absolute para que o texto apareça por cima

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const memeImage = document.getElementById('meme-image');
 
 console.log(memeImage.src);
 input.addEventListener('keyup', () => {
-  // if (memeImage.src === 'http://127.0.0.1:5500/') {
-  //   memeText.style.color = 'black';
-  // } else {
-  //   memeText.style.color = 'white';
-  // }
+  
   memeText.innerText = input.value;
 });
 
 memeInsert.addEventListener('change', () => {
   memeImage.src = URL.createObjectURL(memeInsert.files[0]);
+  if (memeImage.src === 'http://127.0.0.1:5500/') {
+    memeText.style.color = 'black';
+  } else {
+    memeText.style.color = 'white';
+  }
 });
